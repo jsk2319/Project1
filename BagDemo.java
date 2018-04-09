a432c82c3530deb6c3e124cff74abc5c6dc7a9a5
Jude Kallista-Fitzpatrick <31487832+jsk2319@users.noreply.github.com>
2018-04-09 14:35:37 -0400

    Update BagDemo.java

--- BagDemo.java
+++ BagDemo.java
@@ -40,6 +40,7 @@ public class BagDemo {
             }else if (option == 2) {
                 System.out.println("What is the weight of the item?");
                 double newWeight = keyboard.nextDouble();
+                //myBag.addItem(newWeight);
 //else if the user picks option 3, prompt the user for the new maximum
 //weight of the bag and save the new maximum weight in a variable
 //called newMaximumWeight
@@ -47,6 +48,7 @@ public class BagDemo {
             }else if (option == 3) {
                 System.out.println("What should the new maximum weight of the bag be?");
                 double newMaximumWeight = keyboard.nextDouble();
+                //myBag.setMaximumWeight(newMaximumWeight);
 //else if the user picks option 4, display to the screen the name of
 //the bag, the current weight of the bag, and the maximum weight
 //of the bag
